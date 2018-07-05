00bb04f0eeb20bcba1928f4ed4acbf47670d65ff
Takashi Minowa <tm.1201.straycat@gmail.com>
2018-07-05 12:13:55 +0900

    abc100(d未完)

--- src/net/kanolab/tminowa/atcoder/abc/abc099/d/Main.java
+++ src/net/kanolab/tminowa/atcoder/abc/abc099/d/Main.java
@@ -1,21 +1,31 @@
 package net.kanolab.tminowa.atcoder.abc.abc099.d;
 
-import java.util.Map;
+import java.util.Arrays;
 import java.util.Scanner;
-import java.util.SortedMap;
 
+//未達
 public class Main {
-	static Map<Integer, SortedMap<Integer, Integer>> costMap;
-	static int color;
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		int c = sc.nextInt();
-		color = Math.min(3, c);
-		int[][]	 dArray = new int[c][c];
-		int[][]	 cArray = new int[n][n];
+		int pattern = Math.min(3, c);
+		int[][]	dArray = new int[c][c];
+		int[][]	cArray = new int[n][n];
+		int[][] numArray = new int[pattern][c];
+		for(int i = 0; i < pattern; i++) Arrays.fill(numArray[i], 0);
 		for(int i = 0; i < c; i++) for(int j = 0; j < c; j++) dArray[i][j] = sc.nextInt();
 		for(int i = 0; i < n; i++) for(int j = 0; j < n; j++) cArray[i][j] = sc.nextInt();
+		for(int i = 0; i < n; i++) for(int j =0; j < n; j++) numArray[(i + j) % 3][cArray[i][j]-1]++;
+
+		for(int i =0; i < numArray.length; i++) {
+			int min = Integer.MAX_VALUE;
+			for(int j = 0; j< numArray[i].length; j++) {
+				int cost = 0;
+
+			}
+		}
 
 	}
 }
---
+++ src/net/kanolab/tminowa/atcoder/abc/abc100/d/Main.java
@@ -0,0 +1,9 @@
+package net.kanolab.tminowa.atcoder.abc.abc100.d;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+	}
+}
