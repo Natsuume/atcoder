26694587fc8c889c51b805e30ba209cc6a9ef008
Natsuume <Natsuume@NATSUUME-PC>
2018-09-23 13:04:15 +0900

    abc103

---
+++ src/net/kanolab/tminowa/atcoder/abc/abc101_110/abc110/a/Main.java
@@ -0,0 +1,9 @@
+package net.kanolab.tminowa.atcoder.abc.abc101_110.abc110.a;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+	}
+}
---
+++ src/net/kanolab/tminowa/atcoder/abc/abc101_110/abc110/d/Main.java
@@ -0,0 +1,9 @@
+package net.kanolab.tminowa.atcoder.abc.abc101_110.abc110.d;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+	}
+}
