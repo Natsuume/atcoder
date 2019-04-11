c43af6b0d0024dca74aeafc00af07b8a48abebb9
Takashi Minowa <tm.1201.straycat@gmail.com>
2019-04-11 09:00:25 +0900

    abc121 a, b

---
+++ src/net/kanolab/tminowa/atcoder/abc/abc121_130/abc121/b/Main.java
@@ -0,0 +1,26 @@
+package net.kanolab.tminowa.atcoder.abc.abc121_130.abc121.b;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		int c = sc.nextInt();
+		int[] b = new int[m];
+		for(int i=0; i< m; i++)
+			b[i] = sc.nextInt();
+		
+		int count = 0;
+		for(int i= 0; i< n; i++) {
+			int sum = c;
+			for(int j = 0; j <m; j++) 
+				sum += sc.nextInt() * b[j];
+			if(sum > 0)
+				count++;
+		}
+		System.out.println(count);
+		
+	}
+}
---
+++ src/net/kanolab/tminowa/atcoder/abc/abc121_130/abc121/c/Main.java
@@ -0,0 +1,9 @@
+package net.kanolab.tminowa.atcoder.abc.abc121_130.abc121.c;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+	}
+}
