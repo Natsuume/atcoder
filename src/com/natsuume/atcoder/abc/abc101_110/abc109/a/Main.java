5ad583273bb3f67a4f8da17d09c3d1c69831eb31
Natsuume <tm.1201.straycat@gmail.com>
2019-04-15 21:53:09 +0900

    abc109 a-d

---
+++ src/com/natsuume/atcoder/abc/abc101_110/abc109/a/Main.java
@@ -0,0 +1,10 @@
+package com.natsuume.atcoder.abc.abc101_110.abc109.a;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextInt() * sc.nextInt() % 2 == 0 ? "No" : "Yes");
+	}
+}
